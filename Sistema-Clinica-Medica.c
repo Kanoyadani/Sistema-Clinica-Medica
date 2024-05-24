ec5223273a17603eba460bc865f8cff64bf49173
Rodrigo Ryudi Knaoyadani <100811087+Kanoyadani@users.noreply.github.com>
2024-05-24 18:29:46 -0300

    ACABOOOOOOOOOOOOOOOOO!!!!!!
    Código Pronto

--- Sistema-Clinica-Medica.c
+++ Sistema-Clinica-Medica.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -18,6 +19,15 @@ typedef struct no {
     struct no* prox;
 } no;
 
+// Função para limpar o prompt
+void limparTela() {
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+}
+
 // Contador global para os IDs dos pacientes
 int proximoId = 1;
 
@@ -26,6 +36,7 @@ void exibe(no* P) {
     no* atual = P;
     if (atual == NULL) {
         printf("Sem pacientes.\n");
+        system("pause");
         return;
     }
 
@@ -36,9 +47,10 @@ void exibe(no* P) {
         printf("Nome do Paciente: %s\n", atual->nome);
         printf("RG do Paciente: %s\n", atual->rg);
         printf("Prioridade: %d\n", atual->prioridade);
-        printf("----------------------\n");
+        printf("----------------------\n\n");
         atual = atual->prox;
     }
+    system("pause");
 }
 
 // Função para inserir um novo Atendimentos na lista de forma ordenada
@@ -62,24 +74,75 @@ void exibeSug(sug* S) {
     sug* atual = S;
     if (atual == NULL) {
         printf("Sem Atendimentos.\n");
+        system("pause");
         return;
     }
 
-    printf("Lista de Atendimentos:\n");
+    printf("Lista de Atendimentos:\n\n");
     while (atual != NULL) {
         printf("----------------------\n");
         printf("ID do Paciente: %d\n", atual->idp);
         printf("Prioridade: %d\n", atual->priori);
-        printf("----------------------\n");
+        printf("----------------------\n\n");
         atual = atual->proxi;
     }
+    system("pause");
+}
+
+// Função para remover um paciente da lista de pacientes
+no* removePaciente(no* lista, int id) {
+    no* atual = lista;
+    no* anterior = NULL;
+
+    while (atual != NULL && atual->id != id) {
+        anterior = atual;
+        atual = atual->prox;
+    }
+
+    if (atual == NULL) {
+        return lista;  // Paciente não encontrado
+    }
+
+    if (anterior == NULL) {
+        lista = atual->prox;  // Remove o primeiro nó
+    } else {
+        anterior->prox = atual->prox;  // Remove o nó do meio ou do fim
+    }
+
+    free(atual);
+    return lista;
+}
+
+// Função para remover um atendimento da lista de sugestões
+sug* removeSugestao(sug* lista, int id) {
+    sug* atual = lista;
+    sug* anterior = NULL;
+
+    while (atual != NULL && atual->idp != id) {
+        anterior = atual;
+        atual = atual->proxi;
+    }
+
+    if (atual == NULL) {
+        return lista;  // Atendimento não encontrado
+    }
+
+    if (anterior == NULL) {
+        lista = atual->proxi;  // Remove o primeiro nó
+    } else {
+        anterior->proxi = atual->proxi;  // Remove o nó do meio ou do fim
+    }
+
+    free(atual);
+    return lista;
 }
 
 // Função para inserir um paciente no final da lista e criar uma sugestão correspondente
 no* Insere(no* P, sug** S) {
     no* novo = (no*)malloc(sizeof(no));
     if (novo == NULL) {
-        printf("Erro ao alocar memória.\n");
+        printf("Erro ao alocar memoria.\n");
+        system("pause");
         return P;
     }
 
@@ -98,18 +161,18 @@ no* Insere(no* P, sug** S) {
     printf("1- Alto\n");
     printf("2- Medio\n");
     printf("3- Baixo\n");
-    printf("----------------------\n");
+    printf("----------------------\n\n");
     do {
         scanf("%d", &Valor);
         switch (Valor) {
-         case 1: Valor = 1; break;
-         case 2: Valor = 2; break;
-         case 3: Valor = 3; break;
+            case 1: Valor = 1; break;
+            case 2: Valor = 2; break;
+            case 3: Valor = 3; break;
             default: 
                 printf("Valor invalido, Digite um valor Valido\n");
-                Valor = 0;
+                Valor = NULL;
         }
-    } while (Valor == 0);
+    } while (Valor == NULL);
 
     novo->id = proximoId++;
     novo->prioridade = Valor;
@@ -128,7 +191,8 @@ no* Insere(no* P, sug** S) {
     // Criar uma sugestão correspondente e inserir na lista de sugestões
     sug* novaSug = (sug*)malloc(sizeof(sug));
     if (novaSug == NULL) {
-        printf("Erro ao alocar memória.\n");
+        printf("Erro ao alocar memoria.\n");
+        system("pause");
         return P;
     }
 
@@ -137,8 +201,102 @@ no* Insere(no* P, sug** S) {
     novaSug->proxi = NULL;
 
     *S = insertSugInOrder(*S, novaSug);
-
+    system("pause");
     return P;
+}
+
+void atendimentoPrioritario(sug** listaSug, no** listaPac) {
+    int count = 0;
+    sug* atualSug = *listaSug;
+    while (count < 3 && atualSug != NULL) {
+        printf("Atendendo paciente com ID: %d\n", atualSug->idp);
+        printf("Confirme o atendimento do paciente (1 para confirmado): ");
+        int confirmado;
+        scanf("%d", &confirmado);
+        if (confirmado == 1) {
+            *listaPac = removePaciente(*listaPac, atualSug->idp);
+            *listaSug = removeSugestao(*listaSug, atualSug->idp);
+            count++;
+        } else {
+            printf("Paciente nao atendido. Aguardando confirmacao.\n");
+        }
+        system("pause");
+        limparTela();
+        atualSug = *listaSug;
+    }
+
+    if (count == 0) {
+        printf("Nenhum paciente atendido.\n");
+    } else {
+        printf("Atendimento dos 3 primeiros pacientes concluido.\n");
+    }
+    system("pause");
+}
+
+
+void atendimentoNormal(sug** listaSug, no** listaPac) {
+    sug* atualSug = *listaSug;
+    while (atualSug != NULL && atualSug->priori == 1) {
+        atualSug = atualSug->proxi;
+    }
+
+    if (atualSug == NULL) {
+        printf("Nenhum paciente com prioridade diferente de 1 para atendimento.\n");
+        system("pause");
+        return;
+    }
+
+    printf("Atendendo paciente com ID: %d\n", atualSug->idp);
+    printf("Confirme o atendimento do paciente (1 para confirmado): ");
+    int confirmado;
+    scanf("%d", &confirmado);
+    if (confirmado == 1) {
+        *listaPac = removePaciente(*listaPac, atualSug->idp);
+        *listaSug = removeSugestao(*listaSug, atualSug->idp);
+        printf("Atendimento concluido.\n");
+    } else {
+        printf("Paciente nao atendido. Aguardando confirmacao.\n");
+    }
+    system("pause");
+    limparTela();
+}
+
+void atendimento(sug** listaSug, no** listaPac) {
+    if (*listaSug == NULL) {
+        printf("Sem pacientes.\n\n");
+        system("pause");
+        return;
+    }
+
+    int opcao;
+    while (1) {
+        printf("\nSubmenu de Atendimento:\n");
+        printf("1. Atendimento Prioritario\n");
+        printf("2. Atendimento Normal\n");
+        printf("3. Exibir Atendimentos\n");
+        printf("4. Sair\n");
+        printf("Escolha uma opcao: \n\n");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                atendimentoPrioritario(listaSug, listaPac);
+                break;
+            case 2:
+                atendimentoNormal(listaSug, listaPac);
+                break;
+            case 3:
+                printf("Exibir Atendimentos\n\n");
+                exibeSug(*listaSug);
+                limparTela();
+                break;
+            case 4: 
+                return;
+            default:
+                printf("Opcao invalida!\n");
+                system("pause");
+        }
+    }
 }
 
 void atende() {
@@ -146,7 +304,6 @@ void atende() {
     no* lista = NULL;
     // Inicialização da Lista de Sugestões
     sug* listaSug = NULL;
-
     int opcao;
     while (1) {
         printf("\nMenu:\n");
@@ -160,13 +317,15 @@ void atende() {
         switch (opcao) {
             case 1:
                 lista = Insere(lista, &listaSug);
+                limparTela();
                 break;
             case 2:
                 exibe(lista);
+                limparTela();
                 break;
             case 3:
-                 
-                exibeSug(listaSug);
+                atendimento(&listaSug, &lista);
+                limparTela();
                 break;
             case 4: {
                 // Liberar memória dos pacientes
@@ -188,6 +347,7 @@ void atende() {
             }
             default:
                 printf("Opcao invalida!\n");
+                system("pause");
         }
     }
 }
